8b294561eb58714832ba4274612bfa48214516a1
DangDn <doanngocdang96@gmail.com>
2022-11-09 16:30:40 +0700

    update

--- backend/src/lambda/s3/sendNotifications.ts
+++ backend/src/lambda/s3/sendNotifications.ts
@@ -1,61 +1,29 @@
-import { S3Event, S3Handler } from 'aws-lambda'
+import { SNSHandler, SNSEvent, S3Event } from 'aws-lambda'
 import 'source-map-support/register'
-import * as AWS  from 'aws-sdk'
+import { sendMessageToAllClient } from '../utils'
 
-const docClient = new AWS.DynamoDB.DocumentClient()
+export const handler: SNSHandler = async (event: SNSEvent) => {
+  console.log('Processing SNS event ', JSON.stringify(event))
+  for (const snsRecord of event.Records) {
+    const s3EventStr = snsRecord.Sns.Message
+    console.log('Processing S3 event', s3EventStr)
+    const s3Event = JSON.parse(s3EventStr)
 
-const connectionsTable = process.env.CONNECTIONS_TABLE
-const stage = process.env.STAGE
-const apiId = process.env.API_ID
-
-const connectionParams = {
-  apiVersion: "2018-11-29",
-  endpoint: `${apiId}.execute-api.us-east-1.amazonaws.com/${stage}`
+    await processS3Event(s3Event)
+  }
 }
 
-const apiGateway = new AWS.ApiGatewayManagementApi(connectionParams)
-
-export const handler: S3Handler = async (event: S3Event) => {
-  console.log('Processing SNS event ', JSON.stringify(event))
-  for (const record of event.Records) {
+async function processS3Event(s3Event: S3Event) {
+  for (const record of s3Event.Records) {
     const key = record.s3.object.key
-
-  const connections = await docClient.scan({
-      TableName: connectionsTable
-  }).promise()
+    console.log('Processing S3 item with key: ', key)
 
     const payload = {
-      imageId: key
-  }
-
-  for (const connection of connections.Items) {
-      const connectionId = connection.id
-      await sendMessageToClient(connectionId, payload)
-  }
-  }
-}
-
-async function sendMessageToClient(connectionId, payload) {
-  try {
-    console.log('Sending message to a connection', connectionId)
-
-    await apiGateway.postToConnection({
-      ConnectionId: connectionId,
-      Data: JSON.stringify(payload),
-    }).promise()
-
-  } catch (e) {
-    console.log('Failed to send message', JSON.stringify(e))
-    if (e.statusCode === 410) {
-      console.log('Stale connection')
-
-      await docClient.delete({
-        TableName: connectionsTable,
-        Key: {
-          id: connectionId
-        }
-      }).promise()
-
+        imageId: key
     }
+
+    await sendMessageToAllClient(payload)
+    console.log('send imageId to all client: ', key)
   }
 }
+
